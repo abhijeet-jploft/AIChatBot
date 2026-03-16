34bd3e78fddfbcb27097849058a0727db68dc425
Abhijeet <abhijeet_singh@jploft.in>
2026-03-16 16:51:53 +0530

    vite fix for https://chat.tasksplan.com/

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,10 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    port: 3001,
+    allowedHosts: [
+      'chat.tasksplan.com' 
+    ],
     proxy: {
       '/api': {
         target: 'http://localhost:5000',
